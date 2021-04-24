b81199c4c49855a1f95446334cdc16179c8291de
ilaumjd <ilaumjd@gmail.com>
2021-04-24 15:18:53 +0700

    Initial Commit

---
+++ Simple Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Simple Sudoku
+//
+//  Created by aku pintar indonesia on 24/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
