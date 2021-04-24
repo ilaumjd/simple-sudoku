fd3f6985f949a58e1c9d4869f5ef96668c8190a1
ilaumjd <ilaumjd@gmail.com>
2021-04-24 22:05:12 +0700

    rx timer

--- Simple Sudoku/Screens/SudokuViewController.swift
+++ Simple Sudoku/Screens/SudokuViewController.swift
@@ -41,6 +41,7 @@ extension SudokuViewController {
         setupRx_btSolveMe()
         setupRx_cvSudoku()
         setupRxAlert()
+        setupRxTimer()
         
         vm.newGame()
         
@@ -170,6 +171,19 @@ extension SudokuViewController {
                 let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
                 alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
                 self?.present(alert, animated: true)
+            }).disposed(by: disposeBag)
+    }
+    
+    private func setupRxTimer() {
+        let countdown = 30
+        Observable<Int>.interval(.seconds(1), scheduler: MainScheduler.instance)
+            .map { countdown - $0 }
+            .take(until: { $0 == 0 }, behavior: .inclusive)
+            .take(until: self.vm.alert)
+            .subscribe(onNext: { value in
+                print(value)
+            }, onCompleted: {
+                print("completed")
             }).disposed(by: disposeBag)
     }
     
