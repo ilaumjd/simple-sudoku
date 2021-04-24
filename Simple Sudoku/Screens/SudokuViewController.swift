1e1eb41e6640de44b812f6254680dd2597ee54ea
ilaumjd <ilaumjd@gmail.com>
2021-04-24 15:54:45 +0700

    add outlets

--- Simple Sudoku/Screens/SudokuViewController.swift
+++ Simple Sudoku/Screens/SudokuViewController.swift
@@ -9,14 +9,25 @@ import UIKit
 
 class SudokuViewController: UIViewController {
     
+    @IBOutlet weak var lbSudoku: UILabel!
+    @IBOutlet weak var lbTimeTitle: UILabel!
+    @IBOutlet weak var lbTimeValue: UILabel!
+    @IBOutlet weak var btNewGame: UIButton!
+    @IBOutlet weak var btSolveMe: UIButton!
+    @IBOutlet weak var cvSudoku: UICollectionView!
+    @IBOutlet weak var svNumber: UIStackView!
+    
     static func create() -> SudokuViewController {
         return SudokuViewController(nibName: "SudokuViewController", bundle: nil)
     }
+    
+}
+
+// MARK: LIFECYCLE
+extension SudokuViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
 }
