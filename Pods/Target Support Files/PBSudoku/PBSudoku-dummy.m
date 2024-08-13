939602f9c49469cf5b4a9876a3bfca809e9ac78f
ilaumjd <ilaumjd@gmail.com>
2024-08-14 04:44:47 +0700

    remove ignored files

--- Pods/Target Support Files/PBSudoku/PBSudoku-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_PBSudoku : NSObject
-@end
-@implementation PodsDummy_PBSudoku
-@end
--- Pods/Target Support Files/Pods-Simple Sudoku/Pods-Simple Sudoku-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Simple_Sudoku : NSObject
-@end
-@implementation PodsDummy_Pods_Simple_Sudoku
-@end
